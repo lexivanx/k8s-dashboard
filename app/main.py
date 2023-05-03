fe9dd7b1296716014977329860f8163407e4575f
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-05-03 16:08:18 +0300

    Implement user authentication

--- app/main.py
+++ app/main.py
@@ -1,10 +1,57 @@
 import logging
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from app import kubernetes_api, security_benchmark, pod_security_policy, network_policy, container_scanning, runtime_security, compliance_reporting, alerting
 
 app = Flask(__name__)
+app.secret_key = 'your-secret-key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+# Using table named 'users' having 'username' and 'password' columns
+DATABASE = 'your_database.db'
+
+def authenticate_user(username, password):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+    conn.close()
+
+    return user is not None
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if authenticate_user(username, password):
+            user = User(1)
+            login_user(user)
+            return redirect(url_for("dashboard"))
+
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 @app.route("/")
+@login_required
 def dashboard():
     cluster_info = kubernetes_api.get_cluster_info()
     cis_results = security_benchmark.run_cis_benchmark()
