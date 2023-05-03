81b8accb5e08e8ed74a2d77ecc5d4262e50da7e7
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-05-03 15:39:51 +0300

    Implement pagination for Kubernetes API calls

--- app/network_policy.py
+++ app/network_policy.py
@@ -1,8 +1,19 @@
 from kubernetes import client, config
 
-def get_network_policies():
+def get_network_policies(chunk_size=50):
     config.load_kube_config()
     api_instance = client.NetworkingV1Api()
 
-    network_policies = api_instance.list_network_policy_for_all_namespaces()
-    return network_policies
+    continue_token = None
+    all_network_policies = []
+
+    while True:
+        network_policies = api_instance.list_network_policy_for_all_namespaces(limit=chunk_size, _continue=continue_token)
+        all_network_policies.extend(network_policies.items)
+
+        if not network_policies.metadata._continue:
+            break
+
+        continue_token = network_policies.metadata._continue
+
+    return all_network_policies
